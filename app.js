6f1a281af07961d42c6b764a00eabec9b0179785
mickilous <mickilous@gmail.com>
2016-06-03 12:58:12 +0200

    Impl

---
+++ app.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+
+console.log('7', '\t', 'rwx')
+console.log('6', '\t', 'rw-')
+console.log('5', '\t', 'r-x')
+console.log('4', '\t', 'r--')
+console.log('3', '\t', '-wx')
+console.log('2', '\t', '-w-')
+console.log('1', '\t', '--x')
+console.log('0', '\t', '---')
